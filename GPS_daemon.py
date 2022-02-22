79a53c5ed3b7004be98c3cb43b55660802955c3e
Oscar Aguilera <oscar@aguilera.xyz>
2022-02-22 17:19:04 -0300

    Integracion inea

--- GPS_daemon.py
+++ GPS_daemon.py
@@ -8,8 +8,8 @@ cur = miConexion.cursor()
 
 #gps app celu Oscar
 
-unique_id='955034'
-token='gps_inia_imagina12'
+unique_id='9170537657'
+token='gps_inia_1'
 
 cur.execute( "SELECT latitude, longitude,altitude,speed,servertime FROM tc_positions WHERE deviceid=(select id from tc_devices where uniqueid='"+unique_id+"') order by servertime desc limit 1" )
 for latitude, longitude ,altitude,speed,servertime in cur.fetchall() :
@@ -19,36 +19,6 @@ os.system('curl -v -X POST -d "{\"latitude\":'+str(latitude)+'}" iot.igromi.com:
 os.system('curl -v -X POST -d "{\"longitude\":'+str(longitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
 os.system('curl -v -X POST -d "{\"altitude\":'+str(altitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
 os.system('curl -v -X POST -d "{\"speed\":'+str(speed)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-
-#gps app celu Sare
-
-unique_id='338276'
-token='gps_inia2_imagina12'
-
-cur.execute( "SELECT latitude, longitude,altitude,speed,servertime FROM tc_positions WHERE deviceid=(select id from tc_devices where uniqueid='"+unique_id+"') order by servertime desc limit 1" )
-for latitude, longitude ,altitude,speed,servertime in cur.fetchall() :
-    print(latitude, longitude,altitude,speed,servertime)
-
-os.system('curl -v -X POST -d "{\"latitude\":'+str(latitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-os.system('curl -v -X POST -d "{\"longitude\":'+str(longitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-os.system('curl -v -X POST -d "{\"altitude\":'+str(altitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-os.system('curl -v -X POST -d "{\"speed\":'+str(speed)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-
-#gps app celu Aure
-
-unique_id='910813'
-token='gps_inia3_imagina12'
-
-cur.execute( "SELECT latitude, longitude,altitude,speed,servertime FROM tc_positions WHERE deviceid=(select id from tc_devices where uniqueid='"+unique_id+"') order by servertime desc limit 1" )
-for latitude, longitude ,altitude,speed,servertime in cur.fetchall() :
-    print(latitude, longitude,altitude,speed,servertime)
-
-os.system('curl -v -X POST -d "{\"latitude\":'+str(latitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-os.system('curl -v -X POST -d "{\"longitude\":'+str(longitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-os.system('curl -v -X POST -d "{\"altitude\":'+str(altitude)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-os.system('curl -v -X POST -d "{\"speed\":'+str(speed)+'}" iot.igromi.com:8080/api/v1/'+token+'/telemetry --header "Content-Type:application/json"')
-
-
 
 
 
